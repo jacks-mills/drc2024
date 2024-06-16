c677f188ce55bc8e0aa845e935a922033a1c1af9
jamis0 <millsjack002@gmail.com>
2024-06-16 18:35:55 +1000

    Add functionality to create vision thread from control.c

--- control.c
+++ control.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+#include "vision.h"
+
+#define VISION_PRIORITY 80
 
 int main() {
-    printf("Hello, DRC!\n");
+
+    // lock memory to avoid page faults.
+    // See: https://shuhaowu.com/blog/2022/03-linux-rt-appdev-part3.html#virtual-memory-avoid-page-faults-and-use-mlockall
+    if (mlockall(MCL_CURRENT | MCL_FUTURE) == -1) {
+        fprintf(stderr, "mlockall failed\n");
+        exit(1);
+    }
+
+    int error;
+    pthread_t visionThread = vision_create_thread(VISION_PRIORITY, &error);
+    if (error) {
+        return error;
+    }
+
+    pthread_join(visionThread, NULL);
+
     return 0;
 }
