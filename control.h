3cd851db0fb74bad9e407ba50efb49c3f55140a2
jamis0 <millsjack002@gmail.com>
2024-06-23 21:27:02 +1000

    Starting new approach

--- control.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <pthread.h>
-
-pthread_t control_create_thread(int priority);
--- vision.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <pthread.h>
-
-pthread_t vision_create_thread(int priority);
