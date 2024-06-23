c9ac2eeae71a8da69446d4ab04b389442181439b
jamis0 <millsjack002@gmail.com>
2024-06-23 21:52:47 +1000

    Create and initialise control.c and control.h

---
+++ control.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+
+#include "control.h"
+
+
+pid_t control_create_proc(int priority) {
+    return -1;
+}
---
+++ control.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <unistd.h>
+
+pid_t control_create_proc(int priority);
