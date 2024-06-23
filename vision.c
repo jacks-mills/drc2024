9112c9c4696f431a660ff894f635c4e9373af24e
jamis0 <millsjack002@gmail.com>
2024-06-23 21:44:05 +1000

    Create and initialise vision.c and vision.h

---
+++ vision.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+
+#include "vision.h"
+
+
+pid_t vision_create_proc(int priority) {
+    return -1;
+}
---
+++ vision.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <unistd.h>
+
+pid_t vision_create_proc(int priority);
