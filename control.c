65762c98e1789242352b42366b5fbe86f6bd1263
jamis0 <millsjack002@gmail.com>
2024-06-26 13:31:32 +1000

    Add fork and real-time priority setting functionality to control_create_proc() function

--- control.c
+++ control.c
@@ -1,8 +1,30 @@
+#include <stdio.h>
 #include <unistd.h>
+#include <sched.h>
 
 #include "control.h"
 
+static void control_proc();
 
 pid_t control_create_proc(int priority) {
-    return -1;
+    pid_t child = fork();
+    if (child != 0) {
+        // parent process
+        return child;
+    }
+    //child process
+    struct sched_param param;
+    param.sched_priority = priority;
+
+    int error = sched_setscheduler(0, SCHED_FIFO, &param);
+    if (error == -1) {
+        return -1;
+    }
+
+    control_proc();
+
+    return -1; // should never reach here
+}
+
+static void control_proc() {
 }
