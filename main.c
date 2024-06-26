f0352cd3958a834143191771c6ccd0bda79bfeda
jamis0 <millsjack002@gmail.com>
2024-06-26 13:31:55 +1000

    Create control process in main.c

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <sys/wait.h>
 
 #include "control.h"
 #include "vision.h"
 
+int main(int argc, char **argv) {
 
-int main() {
-    printf("Hello, DRC!\n");
+    pid_t controlPid = control_create_proc(80);
+    if (controlPid == -1) {
+        perror("conctrol_create_proc() failed");
+    }
+
+    // wait for all child processes to terminate
+    while (wait(NULL) > -1);
+
     return 0;
 }
