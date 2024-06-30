d74aa56971497bda6f1ba9129552b7aac08fbe45
jamis0 <millsjack002@gmail.com>
2024-06-30 21:19:20 +1000

    Remove vision.c and vision.h files

--- vision.c
+++
@@ -1,8 +0,0 @@
-#include <unistd.h>
-
-#include "vision.h"
-
-
-pid_t vision_create_proc(int priority) {
-    return -1;
-}
--- vision.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <unistd.h>
-
-pid_t vision_create_proc(int priority);
