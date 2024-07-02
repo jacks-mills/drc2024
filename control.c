b4f59d009ef4f2d16c7390edff3a06172fb58145
jamis0 <millsjack002@gmail.com>
2024-07-03 09:22:56 +1000

    Add functionality to receive data from vision process

--- control.c
+++ control.c
@@ -6,19 +6,39 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <mqueue.h>
+#include <semaphore.h>
 
-#define STDOUT_PREFIX "control: "
-#define STDERR_PREFIX "control: "
+#define STDOUT_PREFIX "cont: "
+#define STDERR_PREFIX "cont: "
 
 #define QUEUE_CONT_VISN_DATA_NAME "/DRC-CONT-VISN-DATA"
 #define QUEUE_CONT_VISN_DATA_ELMS 10
 #define QUEUE_CONT_VISN_DATA_SIZE 64
+
+#define SEMAPHORE_CONT_VISN_DATA_NAME "/DRC-CONT-DATA"
+
+#define X_MIN 0
+#define X_MAX 400
+#define Y_MIN 0
+#define Y_MAX 200
+
+struct VisionData {
+    int type;
+    int x;
+    int y;
+};
 
 
 int main(int argc, char **argv) {
     struct mq_attr attr;
     attr.mq_maxmsg = QUEUE_CONT_VISN_DATA_ELMS; 
     attr.mq_msgsize = QUEUE_CONT_VISN_DATA_SIZE; 
+
+//    sem_t *dataReceived = sem_open(
+//        SEMAPHORE_CONT_VISN_DATA_NAME,
+//        O_CREAT,
+//        S_IRWXU,
+//        0);
 
     mqd_t visionQueue = mq_open(
         QUEUE_CONT_VISN_DATA_NAME, 
@@ -31,6 +51,8 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
+    printf(STDOUT_PREFIX "opened \"%s\" queue\n", QUEUE_CONT_VISN_DATA_NAME);
+
     size_t messageLen = 0;
     char message[QUEUE_CONT_VISN_DATA_SIZE];
 
@@ -41,11 +63,18 @@ int main(int argc, char **argv) {
             exit(EXIT_FAILURE);
         }
 
+        struct VisionData data = *((struct VisionData *) message);
+
         printf(
             STDOUT_PREFIX 
-            "Received message \"%s\" from queue \"%s\"\n", 
-            message, 
-            QUEUE_CONT_VISN_DATA_NAME);
+            "From queue \"%s\" received data (%i, %i %i)\n",
+            QUEUE_CONT_VISN_DATA_NAME,
+            data.type,
+            data.x,
+            data.y);
+
+        float turnDegree = 2 * (((float) data.x) - X_MIN) / (X_MAX - X_MIN) - 1;
+        printf(STDOUT_PREFIX "turning degree: %f\n", turnDegree);
     }
 
 }
--- vision.py
+++ vision.py
@@ -1,13 +1,21 @@
-from posix_ipc import MessageQueue
-import struct
+from posix_ipc import *
 from time import sleep
+import struct
+import random
+import sys
 
 QUEUE_CONT_VISN_DATA_NAME = "/DRC-CONT-VISN-DATA"
 QUEUE_CONT_VISN_DATA_ELMS = 10
 QUEUE_CONT_VISN_DATA_SIZE = 64
 
-STDOUT_PREFIX = "vision: "
-STDOUT_PREFIX = "vision: "
+STDOUT_PREFIX = "visn: "
+STDOUT_PREFIX = "visn: "
+
+X_MIN = 0
+X_MAX = 400
+
+Y_MIN = 0
+Y_MAX = 200
 
 def main():
     q = MessageQueue(
@@ -19,10 +27,20 @@ def main():
         read = False,
         write = True)
 
-    count = 0
+    print(STDOUT_PREFIX + "opened \"" + QUEUE_CONT_VISN_DATA_NAME + "\" queue")
+
+    count = 1
     while True:
+        x, y = random.randint(X_MIN, X_MAX), random.randint(Y_MIN, Y_MAX)
+
+        message = struct.pack("iii", 1, x, y)
+
+        q.send(message)
+
+        print(STDOUT_PREFIX + "In queue \"" + QUEUE_CONT_VISN_DATA_NAME + "\" send " + f"({1}, {x}, {y})")
+
         count = count + 1
-        sleep(2)
+        sleep(1)
 
 if (__name__ == "__main__"):
     main()
